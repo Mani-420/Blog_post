53545b22238647b8e97568697fd799c9a7173e38
Mani-420 <tahirabdulrehman46@gmail.com>
2025-05-28 21:30:58 +0500

    Main

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,69 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+
+// Components
+import Navbar from './components/common/Navbar';
+import Footer from './components/common/Footer';
+import ProtectedRoute from './components/common/ProtectedRoute';
+
+// Pages
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
+import CreateBlog from './pages/CreateBlog';
+import EditBlog from './pages/EditBlog';
+import ViewBlog from './pages/ViewBlog';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route
-          path="/edit-blog/:id"
-          element={
-            <ProtectedRoute>
-              <EditBlog />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/create-blog"
-          element={
-            <ProtectedRoute>
-              <CreateBlog />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/view-blog/:id" element={<ViewBlog />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <div className="min-h-screen flex flex-col">
+          <Navbar />
+
+          <main className="flex-grow">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/view-blog/:id" element={<ViewBlog />} />
+
+              <Route
+                path="/dashboard"
+                element={
+                  <ProtectedRoute>
+                    <Dashboard />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/create-blog"
+                element={
+                  <ProtectedRoute>
+                    <CreateBlog />
+                  </ProtectedRoute>
+                }
+              />
+              <Route
+                path="/edit-blog/:id"
+                element={
+                  <ProtectedRoute>
+                    <EditBlog />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </main>
+
+          <Footer />
+        </div>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
