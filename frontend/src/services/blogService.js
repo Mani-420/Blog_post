e2f2febf635a959484c3bca329912762d2d1eef3
Mani-420 <tahirabdulrehman46@gmail.com>
2025-05-28 20:15:46 +0500

    Services

---
+++ frontend/src/services/blogService.js
@@ -0,0 +1,20 @@
+import api from './api';
+
+export const blogService = {
+  // Get all blogs with pagination and filters
+  getAllBlogs: (params = {}) => api.get('/blogs', { params }),
+
+  // Get single blog
+  getBlogById: (id) => api.get(`/blogs/${id}`),
+
+  // User's blogs
+  getUserBlogs: () => api.get('/blogs/user'),
+
+  // CRUD operations
+  createBlog: (blogData) => api.post('/blogs', blogData),
+  updateBlog: (id, blogData) => api.put(`/blogs/${id}`, blogData),
+  deleteBlog: (id) => api.delete(`/blogs/${id}`),
+
+  // Increment views
+  incrementViews: (id) => api.patch(`/blogs/${id}/views`)
+};
