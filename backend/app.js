831d7a1e233a4e6bae6ed3fe52adef9e4ec43c5a
Mani-420 <tahirabdulrehman46@gmail.com>
2025-06-02 19:45:08 +0500

    Main

--- backend/app.js
+++ backend/app.js
@@ -22,14 +22,14 @@ app.use(cookieParser());
 // Routes Import
 import userRoute from './routes/user.route.js';
 import blogRoute from './routes/blogs.route.js';
-import commentRoute from './routes/comments.route.js';
+// import commentRoute from './routes/comments.route.js';
 import reviewRoute from './routes/reviews.route.js';
 
 // Routes Declaration
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/blogs', blogRoute);
-app.use('/api/v1/comments', commentRoute);
-app.use('/api/v1/reviews', reviewRoute);
+// app.use('/api/v1/comments', commentRoute);
+app.use('/api/v1/blogs/:id/reviews', reviewRoute);
 
 // 404 handler for unknown API routes
 app.use((req, res, next) => {
