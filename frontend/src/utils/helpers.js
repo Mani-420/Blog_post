2a8373506259b4ef4de28763e181a689037db796
Mani-420 <tahirabdulrehman46@gmail.com>
2025-06-03 01:25:45 +0500

    Helper Func

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,4 +1,10 @@
-// Add this helper at the top of your file
 export default function stripHtml(html) {
   return html.replace(/<[^>]+>/g, '');
+}
+
+export function shuffleArray(array) {
+  return array
+    .map((value) => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value);
 }
