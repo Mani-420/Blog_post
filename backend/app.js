265eb26586e728af3cd77d7a7a127c23dd4612c3
Mani-420 <tahirabdulrehman46@gmail.com>
2025-06-04 22:37:41 +0500

    Main

--- backend/app.js
+++ backend/app.js
@@ -24,12 +24,13 @@ import userRoute from './routes/user.route.js';
 import blogRoute from './routes/blogs.route.js';
 // import commentRoute from './routes/comments.route.js';
 import reviewRoute from './routes/reviews.route.js';
+import aiRoutes from './routes/ai.routes.js';
 
 // Routes Declaration
 app.use('/api/v1/users', userRoute);
 app.use('/api/v1/blogs', blogRoute);
-// app.use('/api/v1/comments', commentRoute);
 app.use('/api/v1/blogs/:blogId/reviews', reviewRoute);
+app.use('/api/v1/ai', aiRoutes);
 
 // 404 handler for unknown API routes
 app.use((req, res, next) => {
